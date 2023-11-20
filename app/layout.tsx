83074e8837a7815f84e9b003d48e70b085f61876
tonny187 <tonyoanthony@gmail.com>
2023-11-20 18:55:50 -0500

    second commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
             {children}
 
             <footer className='py-10 flex justify-center items-center'>
-              Hecho con ❤️ por la gente de Vercel
+              Make with ❤️ by the people of Vercel
             </footer>
           </body>
     </html>
